9e48f339dacaa7dc87c0b3f200699f5bb0d7a99a
hayaa91 <halomrana23@twq.idserve.net>
2023-11-23 16:25:56 +0300

    Initial Commit

---
+++ mile/mileApp.swift
@@ -0,0 +1,17 @@
+//
+//  mileApp.swift
+//  mile
+//
+//  Created by Haya on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct mileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
